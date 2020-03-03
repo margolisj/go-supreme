c03e0cec052560da7247e02fd9e79b6bb9fdbe1a
Josh Margolis <josh.margolis@takecareof.com>
2020-03-03 01:51:52 -0500

    Adds basic mobile integration test.

---
+++ supremeAPIMobile_integration_test.go
@@ -0,0 +1,27 @@
+// +build integration
+
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetCollectionItemsMobile(t *testing.T) {
+	session := buildTestSession(t)
+
+	item := taskItem{
+		[]string{"temp"},
+		"accessories",
+		"",
+		"blue",
+	}
+	task := testTask()
+	task.Item = item
+	items, err := GetCollectionItemsMobile(session, &task)
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Printf("%+v", items)
+
+}
